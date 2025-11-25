cfef38a61874a927b66968b815f6f40ad4a66952
hjh99 <hjhnicholas@yahoo.com.sg>
2025-11-25 22:33:04 +0800

    memoization and approximation

---
+++ Problem List/Easy/same_tree.py
@@ -0,0 +1,59 @@
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+
+# Example 1:
+
+
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+# Example 2:
+
+
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+# Example 3:
+
+
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+ 
+
+# Constraints:
+
+# The number of nodes in both trees is in the range [0, 100].
+# -104 <= Node.val <= 104
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        queue = deque([(p,q)])
+        while queue:
+            a,b = queue.popleft()
+            if not a and not b:
+                continue
+            if not a or not b or a.val!=b.val:
+                return False
+            
+            queue.append((a.left,b.left))
+            queue.append((a.right,b.right))
+
+        
+        return True
+
+
+        
+        
